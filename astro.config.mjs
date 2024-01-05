3c8f70b3ad692083f73c0c2d5a10a497cf33ba65
M-Shrief <m.shrief.dev@proton.me>
2024-01-05 16:14:22 +0200

    Basic i18n functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "ar"],
+        routing: {
+            prefixDefaultLocale: false
+        },
+        fallback: {
+            ar: "en"
+        }
+    }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,42 @@
+export const languages = {
+    en: 'English',
+    ar: 'العربية',
+  };
+  
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        nav: {
+            "home": "Home",
+            "about": "About"
+        },
+        home: {
+            title: "Welcome to Astro.",
+            h1: 'Home',
+            p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quisquam asperiores ea perferendis maiores eligendi quibusdam fugiat, qui nemo voluptas, unde provident laudantium cumque vel deserunt suscipit error quas? Iste, atque corporis facere autem perspiciatis esse dignissimos saepe, vero nisi harum beatae omnis illum voluptates cumque voluptas numquam. Voluptas hic at, soluta exercitationem reiciendis quidem, amet voluptatum, similique rem adipisci libero. Quos impedit perferendis consequatur debitis laboriosam aut quo possimus saepe, fugit deserunt numquam ad minima beatae? Quae fugit nam harum debitis voluptatem dolorem sapiente sed. Aut dolorum quod quos repellat soluta autem sequi alias fuga ab error, omnis eum.',
+        },
+        about: {
+            title: "About Astro.",
+            h1: 'About',
+            p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quisquam asperiores ea perferendis maiores eligendi quibusdam fugiat, qui nemo voluptas, unde provident laudantium cumque vel deserunt suscipit error quas? Iste, atque corporis facere autem perspiciatis esse dignissimos saepe, vero nisi harum beatae omnis illum voluptates cumque voluptas numquam. Voluptas hic at, soluta exercitationem reiciendis quidem, amet voluptatum, similique rem adipisci libero. Quos impedit perferendis consequatur debitis laboriosam aut quo possimus saepe, fugit deserunt numquam ad minima beatae? Quae fugit nam harum debitis voluptatem dolorem sapiente sed. Aut dolorum quod quos repellat soluta autem sequi alias fuga ab error, omnis eum.',
+
+        }
+    },
+    ar: {
+        // nav: {
+        //     home: "الصفحة الرئيسية",
+        //     about: "من نحن"
+        // },
+        home: {
+            title: "أهلا بك",
+            h1: 'الصفحة الرئيسية',
+            p: `هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء. العديد من برامح النشر المكتبي وبرامح تحرير صفحات الويب تستخدم لوريم إيبسوم بشكل إفتراضي كنموذج عن النص، وإذا قمت بإدخال "lorem ipsum" في أي محرك بحث ستظهر العديد من المواقع الحديثة العهد في نتائج البحث. على مدى السنين ظهرت نسخ جديدة ومختلفة من نص لوريم إيبسوم، أحياناً عن طريق الصدفة، وأحياناً عن عمد كإدخال بعض العبارات الفكاهية إليها.`,
+        },
+        about: {
+            title: "من نحسن",
+            h1: "من نحن",
+            p: `هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء. العديد من برامح النشر المكتبي وبرامح تحرير صفحات الويب تستخدم لوريم إيبسوم بشكل إفتراضي كنموذج عن النص، وإذا قمت بإدخال "lorem ipsum" في أي محرك بحث ستظهر العديد من المواقع الحديثة العهد في نتائج البحث. على مدى السنين ظهرت نسخ جديدة ومختلفة من نص لوريم إيبسوم، أحياناً عن طريق الصدفة، وأحياناً عن عمد كإدخال بعض العبارات الفكاهية إليها.`,
+        },
+    },
+} as const;
